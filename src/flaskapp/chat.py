495bba37f074a04b6aa26f8ce471bcd8f036f4a3
Pamela Fox <pamela.fox@gmail.com>
2023-06-08 15:13:18 -0700

    Updating grader examples

--- src/flaskapp/chat.py
+++ src/flaskapp/chat.py
@@ -33,31 +33,27 @@ def chat_handler():
     # get message from posted JSON
     request_message = request.json["message"]
     response = openai.ChatCompletion.create(
+        request_timeout=30,
         engine=os.getenv("AZURE_OPENAI_CHATGPT_DEPLOYMENT", "chatgpt"),
         messages=[
-            {"role": "system", "content": "You are a teaching assistant for students trying to learn Python programming. Your goal is to provide helpful guidance and encouragement. You should not provide complete code, just provide suggestions for specific lines to fix."},
+            {"role": "system", "content": "You are a helpful assistant that evaluates JavaScript programming projects according to the rubric and criteria given. You should always follow the rubric."},
             {"role": "user", "content": 
             """
 You are evaluating programming projects according to a description and rubric.
-
 Instructions for students:
-
 Project: Fish Tank
 This program draws a single fish. Poor lonely fish! For this project, you'll use functions to accompany him with more fish of all different shapes and colors.
 1. Create a custom function (like drawFish) that draws a fish. You can build on the starter code or start from scratch.
 2. Add at least 2 parameters to the function declaration that control either the position or the size of the fish.
 3. Now call that function many times, with different values as arguments, so that your screen is filled with fish.
-
 Student code:
 ```
 background(89, 216, 255);
-
 var centerX = 203;
 var centerY = 100;
 var bodyLength = 118;
 var bodyHeight = 74;
 var bodyColor = color(162, 0, 255);
-
 noStroke();
 fill(bodyColor);
 // body
@@ -71,8 +67,6 @@ triangle(centerX-bodyLength/2, centerY,
 // eye
 fill(33, 33, 33);
 ellipse(centerX+bodyLength/4, centerY, bodyHeight/5, bodyHeight/5);
-
-
 fill(bodyColor);
 // body
 ellipse(100, 100, bodyLength, bodyHeight);
@@ -86,7 +80,6 @@ triangle(10, centerY,
 fill(33, 33, 33);
 ellipse(centerX+bodyLength/4, centerY, bodyHeight/5, bodyHeight/5);
  
-
 fill(0, 173, 55);
 ellipse(74, 406, 10, 200);
 ellipse(88, 434, 10, 200);
@@ -98,18 +91,15 @@ ellipse(177, 429, 10, 200);
 ellipse(239, 411, 10, 200);
 ```
 
+For the code above, fill out the grading rubric below. If all criteria are met (Yes), the project passes evaluation.
 
-Grading criteria:
-(Yes/No) Contains a function that can draw a fish or some other object.
+*** GRADING RUBRIC ***
+This project (does / does not) meet the grading criteria.
 
+(Yes/No): Contains a function that can draw a fish or some other object.
 (Yes/No): Must pass in at least two arguments to that function to control position or size.
-
 (Yes/No): Must call that function at least three times or in response to user interaction, to draw multiple objects around the canvas.
-
 (Yes/No): Doesn't have any syntax errors or program logic errors.
-
-If all criteria are met (Yes), the project passes evaluation.
-Please fill out the grading criteria for the student program above.
             """ 
             },
             {"role": "assistant", "content": """
@@ -122,22 +112,17 @@ Yes: Doesn't have any syntax errors or program logic errors."""
             },
             {"role": "user", "content": """
 You are evaluating programming projects according to a description and rubric.
-
 Instructions for students:
-
 Project: Fish Tank
 This program draws a single fish. Poor lonely fish! For this project, you'll use functions to accompany him with more fish of all different shapes and colors.
 1. Create a custom function (like drawFish) that draws a fish. You can build on the starter code or start from scratch.
 2. Add at least 2 parameters to the function declaration that control either the position or the size of the fish.
 3. Now call that function many times, with different values as arguments, so that your screen is filled with fish.
-
 Student code:
 ```
 background(89, 216, 255);
 var salad = function(centerX,centerY,bodyLength,bodyColor){
-
 var bodyHeight = bodyLength/2;
-
 noStroke();
 fill(bodyColor);
 // body
@@ -157,9 +142,7 @@ salad(169,212,160, color(68, 98, 158));
 salad(259,66,77, color(199, 84, 103));
 salad(193,249,92, color(168, 62, 122));
 salad(116,127,127, color(196, 29, 76));
-
 var PEB=function(E,S){
-
 //ZE PEBBLES
 stroke(64, 61, 64);
 fill(166, 61, 124);
@@ -174,23 +157,19 @@ PEB(205,100);
 PEB(364,100);
 ```
 
-Grading criteria:
-(Yes/No) Contains a function that can draw a fish or some other object.
+For the code above, fill out the grading rubric below. If all criteria are met (Yes), the project passes evaluation.
 
+*** GRADING RUBRIC ***
+This project (does / does not) meet the grading criteria.
+
+(Yes/No): Contains a function that can draw a fish or some other object.
 (Yes/No): Must pass in at least two arguments to that function to control position or size.
-
 (Yes/No): Must call that function at least three times or in response to user interaction, to draw multiple objects around the canvas.
-
 (Yes/No): Doesn't have any syntax errors or program logic errors.
-
-If all criteria are met (Yes), the project passes evaluation.
-Please fill out the grading criteria for the student program above
-and provide suggestions for next steps.                       
 """
             },
             {"role": "assistant", "content": """
-Result: This project does not meet the grading criteria.
-Suggestion: Make sure you put your code inside functions! Keep working on it.
+This project does meet the grading criteria.
 
 Yes: Contains a function that can draw a fish or some other object.
 Yes: Must pass in at least two arguments to that function to control position or size.
@@ -199,31 +178,26 @@ Yes: Doesn't have any syntax errors or program logic errors."""
             },
             {"role": "user", "content": """
 You are evaluating programming projects according to a description and rubric.
-
 Instructions for students:
-
 Project: Fish Tank
 This program draws a single fish. Poor lonely fish! For this project, you'll use functions to accompany him with more fish of all different shapes and colors.
 1. Create a custom function (like drawFish) that draws a fish. You can build on the starter code or start from scratch.
 2. Add at least 2 parameters to the function declaration that control either the position or the size of the fish.
 3. Now call that function many times, with different values as arguments, so that your screen is filled with fish.
-
 Student code:
 ```
 REQUEST_MESSAGE
 ```
 
-Grading criteria:
-(Yes/No) Contains a function that can draw a fish or some other object.
+For the code above, fill out the grading rubric below. If all criteria are met (Yes), the project passes evaluation.
 
+*** GRADING RUBRIC ***
+This project (does / does not) meet the grading criteria.
+
+(Yes/No): Contains a function that can draw a fish or some other object.
 (Yes/No): Must pass in at least two arguments to that function to control position or size.
-
 (Yes/No): Must call that function at least three times or in response to user interaction, to draw multiple objects around the canvas.
-
-(Yes/No): Doesn't have any syntax errors or program logic errors.
-
-If all criteria are met (Yes), the project passes evaluation.
-Please fill out the grading criteria for the student program above.           
+(Yes/No): Doesn't have any syntax errors or program logic errors.         
 """.replace("REQUEST_MESSAGE", request_message)},
         ],
         stream=False,
